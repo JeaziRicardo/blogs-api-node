1a09570d25f09bf694ce69afb715ca9ab41f45ec
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-09-01 17:08:01 -0300

    feat: cria schema para validar 'user'

--- src/middlewares/schema.js
+++ src/middlewares/schema.js
@@ -1,14 +1,40 @@
 const Joi = require('joi');
 
+const KEY_EMPTY = '400|Some required fields are missing';
+
 const login = Joi.object().keys({
   email: Joi.string().empty().required().messages({
-    'string.empty': '400|Some required fields are missing',
+    'string.empty': KEY_EMPTY,
     'any.required': '400|email is required',
   }),
   password: Joi.string().empty().required().messages({
-    'string.empty': '400|Some required fields are missing',
+    'string.empty': KEY_EMPTY,
     'any.required': '400|password is required',
   }),
 });
 
-module.exports = { login };
+const user = Joi.object().keys({
+  displayName: Joi.string().empty().min(8).required()
+    .messages({
+      'string.empty': KEY_EMPTY,
+      'string.min': '400|"displayName" length must be at least {#limit} characters long',
+      'any.required': '400|displayName is required',
+  }),
+  email: Joi.string().empty().email().required()
+    .messages({
+      'string.empty': KEY_EMPTY,
+      'string.email': '400|"email" must be a valid email',
+      'any.required': '400|email is required',
+  }),
+  password: Joi.string().empty().min(6).required()
+    .messages({
+      'string.empty': KEY_EMPTY,
+      'string.min': '400|"password" length must be at least {#limit} characters long',
+      'any.required': '400|password is required',
+  }),
+});
+
+module.exports = {
+  login,
+  user,
+};
