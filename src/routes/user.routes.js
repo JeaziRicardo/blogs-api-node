be9d977c8f61c55875c7dc6a3bcdbeab8d9f79aa
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-09-01 18:29:18 -0300

    feat: exibe os usuários no endpoint GET /user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const user = require('../controller/user.controller');
+const validate = require('../middlewares/auth');
 
 const userRouter = Router();
 
+userRouter.get('/user', validate.auth, user.getAll);
 userRouter.post('/login', user.login);
 userRouter.post('/user', user.create);
 
