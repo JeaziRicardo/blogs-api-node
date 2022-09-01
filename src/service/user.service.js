08c612d37b3d4f69890fcde5348a3440ec662d44
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-09-01 18:27:19 -0300

    feat: service para exibir todos usuários

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -31,7 +31,14 @@ const create = async ({ displayName, email, password, image }) => {
   return token;
 };
 
+const getAll = async () => {
+  const users = await User.findAll({ attributes: { exclude: ['password'] } });
+
+  return users;
+};
+
 module.exports = {
   login,
   create,
+  getAll,
 };
