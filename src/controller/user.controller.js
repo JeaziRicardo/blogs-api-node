664f70ce2604f25347d955c84c866f425fd06b68
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-09-01 09:31:25 -0300

    fiz: remove o try/catch do controller 'user'

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,16 +1,11 @@
-const CustomError = require('../errors/customError');
 const user = require('../service/user.service');
 
 const login = async (req, res) => {
   const { email, password } = req.body;
 
-  try {
-    const token = await user.login({ email, password });
-    res.status(200).json({ token });
-  } catch (error) {
-    console.log('error: ', error);
-    throw new CustomError();
-  }
+  const token = await user.login({ email, password });
+  
+  res.status(200).json({ token });
 };
 
 module.exports = { login };
