2db3eaf6ae5b653fe277131984fc5826a7cd2026
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-09-01 17:15:05 -0300

    feat: controller que cria um 'user'

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -8,4 +8,15 @@ const login = async (req, res) => {
   res.status(200).json({ token });
 };
 
-module.exports = { login };
+const create = async (req, res) => {
+  const { displayName, email, password, image } = req.body;
+
+  const token = await user.create({ displayName, email, password, image });
+
+  res.status(201).json({ token });
+};
+
+module.exports = {
+  login,
+  create,
+};
