328323261ac169a00535c8eb343f16638771c641
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-09-01 17:16:15 -0300

    feat: cria o endpoint POST em '/user'

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,5 +4,6 @@ const user = require('../controller/user.controller');
 const userRouter = Router();
 
 userRouter.post('/login', user.login);
+userRouter.post('/user', user.create);
 
 module.exports = userRouter;
