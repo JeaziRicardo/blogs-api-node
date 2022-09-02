c37440773d7f3a81b1afc4d494b9632d79787d91
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-09-02 20:52:22 -0300

    feat: cria o service para listar um post pelo id

--- src/service/post.service.js
+++ src/service/post.service.js
@@ -1,4 +1,5 @@
 const { BlogPost, User, Category } = require('../database/models');
+const CustomError = require('../errors/customError');
 
 const getAll = async () => {
   const posts = await BlogPost.findAll({
@@ -11,6 +12,21 @@ const getAll = async () => {
   return posts;
 };
 
+const getById = async (id) => {
+  const post = await BlogPost.findOne({
+    where: { id },
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Category, as: 'categories', through: { attributes: [] } },
+    ],
+  });
+
+  if (!post) throw new CustomError(404, 'Post does not exist');
+
+  return post;
+};
+
 module.exports = {
   getAll,
+  getById,
 };
