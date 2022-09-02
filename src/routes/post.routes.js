3bc5a13c0204c5b2cf95a2fe369d175baebdbd75
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-09-02 20:53:31 -0300

    feat: cria o endpoint GET em '/post/:id'

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -5,5 +5,6 @@ const validate = require('../middlewares/auth');
 const postRouter = Router();
 
 postRouter.get('/post', validate.auth, controller.getAll);
+postRouter.get('/post/:id', validate.auth, controller.getById);
 
 module.exports = postRouter;
