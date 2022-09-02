edf601daae1ea92aae5b9ed470d255bd8e6ef85f
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-09-01 21:42:47 -0300

    feat: camada controller para exibir as categorias

--- src/controller/category.controller.js
+++ src/controller/category.controller.js
@@ -8,6 +8,13 @@ const create = async (req, res) => {
   res.status(201).json(category);
 };
 
+const getAll = async (_req, res) => {
+  const categories = await service.getAll();
+
+  res.status(200).json(categories);
+};
+
 module.exports = {
   create,
+  getAll,
 };
